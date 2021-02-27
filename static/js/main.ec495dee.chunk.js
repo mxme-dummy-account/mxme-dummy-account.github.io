894d6d20e01aa79364f02b4f674ed93c05254bea
Martin <24744@student.hhs.se>
2021-02-27 15:45:23 +0100

    Initial version

---
+++ static/js/main.ec495dee.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[0],{11:function(n,t,e){"use strict";e.r(t);e(1),e(4),e(8),e.p,e(9),e(0);(function(n){n&&n instanceof Function&&e.e(3).then(e.bind(null,12)).then((function(t){var e=t.getCLS,i=t.getFID,c=t.getFCP,s=t.getLCP,o=t.getTTFB;e(n),i(n),c(n),s(n),o(n)}))})()},8:function(n,t,e){},9:function(n,t,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.ec495dee.chunk.js.map
